9809c0839094048b93c7e5a06cd7741e226c4d6a
rownox <rownox.ik@gmail.com>
2022-07-23 20:41:26 -0400

    gmc command

---
+++ src/main/java/net/blocksumo/Commands/gamemodeCMD.java
@@ -0,0 +1,17 @@
+package net.blocksumo.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class gamemodeCMD implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (sender instanceof Player p) {
+
+        }
+        return false;
+    }
+}
