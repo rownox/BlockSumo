4216b6a4e0beead3fbebb708e6745d4066fd7c5a
rownox <rownox.ik@gmail.com>
2022-07-23 20:30:41 -0400

    g

---
+++ src/main/java/net/blocksumo/BlockSumo.java
@@ -0,0 +1,17 @@
+package net.blocksumo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BlockSumo extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
